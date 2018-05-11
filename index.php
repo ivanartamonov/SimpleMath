1fc265fa002da9675dbc2fb59feafddae756db93
Ivan Artamonov <i.o.artamonov@gmail.com>
2018-05-12 00:00:23 +0300

    travis

--- index.php
+++ index.php
@@ -1,22 +1,3 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-
-use SimpleMath\Matrix;
-
-$dataA = [
-	[1, 2, 3],
-	[3, 4, 5],
-];
-
-$dataB = [
-	[2, 3],
-	[4, 5],
-	[2, 3],
-];
-
-$A = new Matrix($dataA);
-$B = new Matrix($dataB);
-
-$C = $A->multiplyMatrix($B);
-$C->printMatrix();
